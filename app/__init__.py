3c225d9fbe4f26a89040db0b4a8bd49ffe085015
vugutsa <vugutsamercy84@gmail.com>
2020-12-03 08:54:40 +0300

    initialize imports to the __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,47 +1,47 @@
-# from flask import Flask
-# from config import config_options
-# from flask_bootstrap import Bootstrap
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
-# from flask_uploads import UploadSet,configure_uploads,IMAGES
-# from flask_mail import Mail
-# from flask_simplemde import SimpleMDE
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
-# mail = Mail()
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
-# db = SQLAlchemy()
-# photos = UploadSet('photos',IMAGES)
-# simple = SimpleMDE()
-# def create_app(config_name):
-#     app = Flask(__name__)
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
+def create_app(config_name):
+    app = Flask(__name__)
 
-#     # Creating the app configurations
-#     app.config.from_object(config_options[config_name])
-#     from .auth import auth as auth_blueprint
-#     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     
-#     mail.init_app(app)
-#     simple.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
     
-#     # configure UploadSet
-#     configure_uploads(app,photos
+    # configure UploadSet
+    configure_uploads(app,photos
     
-#     # Initializing flask extensions
-#     bootstrap.init_app(app)
-#     db.init_app(app)
-#     login_manager.init_app(app)
-#     # Registering the blueprint
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
    
 
-#     # setting config
-#     from .requests import configure_request
-#     configure_request(app)
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
-#     return app
+    return app
